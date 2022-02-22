f7fa8196eee7b124559b9d76c9cd9a688dd52b83
Situn <ksaswat007@gmail.com>
2022-02-22 15:55:40 +0000

    used dotenv for connection URL

--- config/db.js
+++ config/db.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 
 function connectDB(){
-    mongoose.connect(url, { useNewUrlParser: true, useCreateIndex:true, useUnifiedTopology: true, useFindAndModify: true});
+    mongoose.connect(process.env.MONGO_CONNECTION_URL, { useNewUrlParser: true, useCreateIndex:true, useUnifiedTopology: true, useFindAndModify: true});
     const connection = mongoose.connection;
 
     connection.once('open', () => {
