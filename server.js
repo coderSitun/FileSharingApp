5e50c7047e24cefa9fa626f3a0119deb92c315b0
Situn <ksaswat007@gmail.com>
2022-02-22 18:00:41 +0000

    Initialized database connection

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const app = express();
 
 const PORT = 12345;
 
+const connectDB = require('./config/db');
+connectDB();
+
 app.listen(PORT, () => {
     console.log('Listening on port ' + PORT);
 })
