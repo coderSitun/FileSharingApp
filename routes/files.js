cdaf33d5b0ce28c56741668d349ad49118cc0d37
Situn <ksaswat007@gmail.com>
2022-02-22 18:37:10 +0000

    Multer use for storage

--- routes/files.js
+++ routes/files.js
@@ -1,4 +1,12 @@
 const router = require('express').Router();
+const multer = require('multer');
 
+let storage = multer.diskStorage({
+    destination: (req, file, cb) => cb(null, 'uploads/'),
+    filename: (req, file, cb) => {
+        const uniqueName = Date.now() + Math.round(Math.random() * 1E9) + path.extname(file.originalname);
+        cb(null, uniqueName);
+    }
+});
 
 module.exports = router;
