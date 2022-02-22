ad7984dda76d407959f7457550508fee4d206937
Situn <ksaswat007@gmail.com>
2022-02-22 18:41:16 +0000

    Fixing compilation error

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const connectDB = require('./config/db');
 connectDB();
 
 //Routes
-app.use('/api/files', require('.routes/files'));
+app.use('/api/files', require('./routes/files'));
 
 app.listen(PORT, () => {
     console.log('Listening on port ' + PORT);
